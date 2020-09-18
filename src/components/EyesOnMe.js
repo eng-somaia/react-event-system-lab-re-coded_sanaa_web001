c5888ceb10295da24f9ed910acb4ffa4170dd2a7
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-18 20:57:22 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
-// Code EyesOnMe Component Here
+import React, { Component } from 'react';
+
+export default class EyesOnMe extends Component {
+focusOn = ()=> {
+    cosole.log('Good!')
+  }
+focusOff = ()=> {
+    cosole.log('Hey! Eyes on me!')
+  }
+render() {
+    return (
+    <div >
+    <button focus= {this.focusOn} blur= {this.focusOff}>Focus?</button>
+    </div>
+    )
+}
+}
